887c51cada0367e534b19e4860efc3406550a697
tayyip <tayyipgedik66@gmail.com>
2026-02-09 21:09:03 +0100

    tstprod

--- src/components/map/MarkerClusterGroup.tsx
+++ src/components/map/MarkerClusterGroup.tsx
@@ -1,12 +1,15 @@
 import { createLayerComponent } from '@react-leaflet/core';
 import type { LeafletContextInterface } from '@react-leaflet/core';
-import L, { MarkerClusterGroupOptions } from 'leaflet';
+import type { ReactNode } from 'react';
+import L from 'leaflet';
+import type { MarkerClusterGroupOptions } from 'leaflet';
 import 'leaflet.markercluster';
 
 export type MarkerClusterProps = MarkerClusterGroupOptions & {
   iconCreateFunction?: (cluster: any) => L.DivIcon;
   chunkedLoading?: boolean;
   showCoverageOnHover?: boolean;
+  children?: ReactNode;
 };
 
 const createMarkerClusterGroup = (props: MarkerClusterProps, context: LeafletContextInterface) => {
